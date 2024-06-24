7d2307573e3d70d84b884c99ccd2c7801010c474
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-24 08:25:14 +0300

    Add lissajous to http server

--- ch1/server1.go
+++ ch1/server1.go
@@ -2,46 +2,59 @@
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
-	"sync"
 )
 
-var mu sync.Mutex
-var count int
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // second color in palette
+)
 
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler
-	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular revolution
+		size    = 100   // image canvas covers [-size ... +size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
 
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "%q = %q\n", k, v)
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
 
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-
-	fmt.Fprintf(w, "Remote address = %q\n", r.RemoteAddr)
-
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
-	}
-
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
-
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
 
-func counter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%d\n", count)
+func handler(w http.ResponseWriter, r *http.Request) {
+	lissajous(w)
 }
