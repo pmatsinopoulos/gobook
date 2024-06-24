b530e032dfa2220e8ad359b5142e0092ab4720a8
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-06-24 08:16:25 +0300

    Parsing form data

--- ch1/server1.go
+++ ch1/server1.go
@@ -18,6 +18,24 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "%q = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+
+	fmt.Fprintf(w, "Remote address = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+
 	mu.Lock()
 	count++
 	mu.Unlock()
